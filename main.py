13a3212e40a4220db52177f46770583f33ef32e8
mikechess <mikecook450@gmail.com>
2025-08-10 16:37:17 -0400

    Add more pages to PDF

--- main.py
+++ main.py
@@ -11,6 +11,9 @@ for index, row in df.iterrows():
     pdf.set_text_color(100, 100, 100)
     pdf.cell(w=0, h=12, txt=row["Topic"], ln=1, align='L')
     pdf.line(10, 21, 200, 21)
+    
+    for i in range(row["Pages"] - 1):
+        pdf.add_page()
 
 
 pdf.output('output.pdf')
