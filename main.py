004710aa64577dbf4f7433840943f68f32a3c2f9
mikechess <mikecook450@gmail.com>
2025-08-10 15:30:00 -0400

    Create a Multipage PDF

--- main.py
+++ main.py
@@ -1,1 +1,25 @@
+from fpdf import FPDF
+
+pdf = FPDF(orientation='P', unit='mm', format='A4')
+
+pdf.add_page()
+
+pdf.set_font(family="Times", style="B", size=12)
+pdf.cell(w=0, h=12, txt='Hello There!', ln=1, align='L')
+
+pdf.set_font(family="Times", size=10)
+pdf.cell(w=0, h=12, txt='Hi There!', ln=1, align='L')
+
+pdf.add_page()
+
+pdf.set_font(family="Times", style="B", size=12)
+pdf.cell(w=0, h=12, txt='Hello There!', ln=1, align='L')
+
+pdf.set_font(family="Times", size=10)
+pdf.cell(w=0, h=12, txt='Hi There!', ln=1, align='L')
+
+pdf.output('output.pdf')
+
+
+
 
